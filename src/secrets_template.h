3be317f96d1a6db16b93000c459250f077af4a4b
Francesco-Pagano <frank.pagano@protonmail.ch>
2025-09-15 22:28:08 +0200

    First commit

---
+++ src/secrets_template.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+#define WIFI_SSID "INSERT_YOUR_WIFI_SSID" // SSID WiFi
+#define WIFI_PASSWORD "INSERT_YOUR_WIFI_PASSWORD"   // Password WiFi
+
+#define PROJECT_ID "INSERT_YOUR_PROJECT_ID" // Google Project ID
+#define CLIENT_EMAIL "INSERT_YOUR_CLIENT_EMAIL" // Service Account's client email
+const char PRIVATE_KEY[] PROGMEM = "INSERT_YOUR_PRIVATE_KEY"; // Service Account's private key
+const char spreadsheetId[] = "INSERT_YOUR_spreadsheetId"; // The ID of the spreadsheet where you'll publish the data
